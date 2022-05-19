7c565c7f545273c2144adf847e49717ef882f59f
Matthew Wong <matthew.jay.wong@gmail.com>
2022-05-19 13:52:54 -0400

    Updates

--- src/App/Views/About/About.jsx
+++ src/App/Views/About/About.jsx
@@ -11,9 +11,9 @@ export default function About() {
     >
       <h2>About Me</h2>
       <p>
-        Like I said before, My name is Matt and I've been working in tech, specifically capitol markets within the banks for over 2.5 years. 
+        Like I said before, My name is Matt and I work in tech as a full-stack developer.
         I have done everything from doing whatever I can to quickly get new projects off the ground (helping with back + front end, initial pipeline and infrastructure setup), 
-        to quickly familiarizing myself with large code bases in order to bring value to the project as soon as possible. While red tape exists to slow down development, I've alwys tried my best to never let this be the reason I cannot progress with development by opening a dialog with them, I am always keen on
+        to quickly familiarizing myself with large code bases in order to bring value to the project as soon as possible. While red tape exists to slow down development, I've always tried my best to never let this be the reason I cannot progress with development by opening a dialog with them.
       </p>
       <p>
         If you or anyone you know has a cool project or gig in mind that you think I would be perfect for, 
--- src/App/Views/Home/Home.jsx
+++ src/App/Views/Home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       id="home"
     >
       <p>
-        Hey! My name is Matt. I am currently working full-time as a full-stack developer at <ExternalLink url="https://www.rbccm.com/en/">RBC Capitol Markets</ExternalLink>.
+        Hey! My name is Matt. I currently work as a full-stack developer at <ExternalLink url="https://www.linkedin.com/company/bookjane">BookJane</ExternalLink>. 
         I like finding scalable and maintainable solutions for any problem space so if you have any good work, let's make something happen! 
         You can read more about me and what I have to offer <a href="#about">here</a>. 
         My resume can also be found <ExternalLink url={`${process.env.PUBLIC_URL}/software_developer_resume.pdf`}>here</ExternalLink>. 
