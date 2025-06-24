4b18c85030279f5f5ba4da9e13ca3a00f4403954
matthew-jay-wong <matthew.jay.wong@gmail.com>
2025-06-24 13:01:10 -0400

    Test branch protection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Testing Branch Protection Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
